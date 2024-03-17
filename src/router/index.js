481bd9c098176407b8db3af33a82bf20935fe52a
zhao-leo <18055219130@163.com>
2024-03-17 22:00:58 +0800

    Delete Tree

--- src/router/index.js
+++ src/router/index.js
@@ -56,25 +56,15 @@ export const constantRoutes = [
   },
 
   {
-    path: '/example',
+    path: '/table',
     component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'el-icon-s-help' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
+    name: '建议和诉求',
+    children: [{
+      path: 'table',
+      name: 'Table',
+      component: () => import('@/views/table/index'),
+      meta: { title: 'Table', icon: 'table' }
+    }]
   },
 
   {
